03fb0060b3d802383d0608e62eba5d9eb7f8ef5d
Lucas Cristi <lucasccristi@gmail.com>
2022-06-20 23:44:52 -0300

    Feito IF para comparações no arquivo de futebol e alterado arquivo de IMC para JS.

--- Lógica/futebol.js
+++ Lógica/futebol.js
@@ -4,6 +4,19 @@ var empates = parseInt(prompt("Número de empates."));
 
 var pontos = (vitorias * 3) + empates
 
+
 function retorna() {
     document.getElementById("pontos").value = pontos
+    if (pontos > 28) {
+
+        alert("Seu time está melhor que o ano passado")
+    }
+    if (pontos < 28) {
+
+        alert("Seu time está pior que o ano passado")
+    }
+    if (pontos == 28) {
+
+        alert("Seu time está igual que o ano passado")
+    }
 }
--- Lógica/imc.js
+++ Lógica/imc.js
@@ -1,37 +1,48 @@
 
-// FUNCAO COM VAR 
+// // FUNCAO COM VAR 
+// function calculaImc(peso, altura) {
+//     return peso / (altura * 2);
+// }
+
+// var imcLucas = calculaImc(70, 1.80);
+// var imcAmigo = calculaImc(80, 1.70);
+
+// console.log(`O seu IMC é ${imcLucas.toFixed(2)}`);
+// console.log(`O IMC do seu amigo é ${imcAmigo.toFixed(2)}`);
+
+// // FUNCAO INFORMANDO PESO E ALTURA DIRETA NO RETURN
+
+// function calculaImc(peso, altura) {
+//     return peso / (altura * 2);
+// }
+
+// console.log(`O seu IMC é ${calculaImc(70, 1.80).toFixed(2)}`);
+// console.log(`O IMC do seu amigo é ${calculaImc(80, 1.70).toFixed(2)}`);
+
+// // OUTRA FORMA
+
+// function calculaImc(peso, altura) {
+//     return peso / (altura * 2);
+// }
+
+// var alturaInformada = 1.80;
+// var pesoInformado = 70;
+// var imc = calculaImc(pesoInformado, alturaInformada);
+
+// console.log(`O seu IMC é ${imc.toFixed(2)}`);
+
+
 function calculaImc(peso, altura) {
     return peso / (altura * 2);
 }
 
-var imcLucas = calculaImc(70, 1.80);
-var imcAmigo = calculaImc(80, 1.70);
-
-console.log(`O seu IMC é ${imcLucas.toFixed(2)}`);
-console.log(`O IMC do seu amigo é ${imcAmigo.toFixed(2)}`);
-
-// FUNCAO INFORMANDO PESO E ALTURA DIRETA NO RETURN
-
-function calculaImc(peso, altura) {
-    return peso / (altura * 2);
-}
-
-console.log(`O seu IMC é ${calculaImc(70, 1.80).toFixed(2)}`);
-console.log(`O IMC do seu amigo é ${calculaImc(80, 1.70).toFixed(2)}`);
-
-// OUTRA FORMA
-
-function calculaImc(peso, altura) {
-    return peso / (altura * 2);
-}
-
-var alturaInformada = 1.80;
-var pesoInformado = 70;
+var nome = prompt("Informe seu nome.")
+var alturaInformada = prompt(`${nome}, informe a sua altura.`);
+var pesoInformado = prompt(`${nome}, informe o seu peso.`);
 var imc = calculaImc(pesoInformado, alturaInformada);
 
-console.log(`O seu IMC é ${imc.toFixed(2)}`);
-
-
-
+function mostra() {
+    document.getElementById("imc").value = imc.toFixed(2)
+}
 
 
