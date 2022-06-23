094e726c588ac53523b160399e67545662209c64
Lucas Cristi <lucasccristi@gmail.com>
2022-06-23 13:53:13 -0300

    While - contador

--- Lógica/numeropensado/numeroPensado.js
+++ Lógica/numeropensado/numeroPensado.js
@@ -1,10 +1,14 @@
-var numeroPensado = Math.round(Math.randonm() * 10);
-var chute = parseInt(prompt("Digite seu chute!"))
+function sorteia(n) {
+    return Math.round(Math.random() * n);
+}    
 
+var numeroPensado = sorteia(82)
+console.log(numeroPensado)
+var chute = parseInt(prompt("Digite seu chute!"))
 if (chute == numeroPensado) {
     document.write("Voçê acertou!")
-} else {
-    document.write("Voçê errou, o número pensado foi " + numeroPensado)
+} else if (chute > numeroPensado) {
+    document.write("Voçê errou, o número era menor")
+} else if (chute < numeroPensado) {
+    document.write("Você errou, o número era maior")
 }
-
-
---
+++ anoCopa/anocopa.js
@@ -0,0 +1,25 @@
+var contador = 30;
+
+while(contador <= 40) {
+
+    if( contador == 33 ) {
+    contador = contador + 1;
+    }
+    else if(contador == 37) {    
+        contador = contador + 1;
+    }
+    else {
+    console.log(contador);
+    contador = contador + 1; 
+    }
+  }
+console.log("FIM")
+
+var contador = 20;
+
+while(contador >= 0) {
+    
+    console.log(contador)
+    contador = contador -1
+  }
+console.log("FIM")
